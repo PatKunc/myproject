225ca5a44fbf5a057cd30cad79f97486659768b9
Pattarakorn <pattarakorn.kunc@kmutt.ac.th>
2022-03-23 12:12:05 +0700

    mdver2

---
+++ Myproject.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class Myproject {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Yes Sir");
+    }
+    
+}
