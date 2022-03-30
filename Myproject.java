9c30eccb6624a3d37a328638961daae20b4b795a
Pattarakorn <pattarakorn.kunc@kmutt.ac.th>
2022-03-30 09:57:59 +0700

    3-committed

--- Myproject.java
+++ Myproject.java
@@ -7,6 +7,7 @@ public class Myproject {
         System.out.println("Hello world");
         System.out.println("Yes Sir");
         System.out.println("Test");
+        System.out.println("Java Programming");
     }
     
 }
