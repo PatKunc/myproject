9dfc3dff39f3f661461aef6207d1522347d08b4f
Pattarakorn <pattarakorn.kunc@kmutt.ac.th>
2022-03-30 09:43:41 +0700

    2-committed

--- Myproject.java
+++ Myproject.java
@@ -6,6 +6,7 @@ public class Myproject {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Yes Sir");
+        System.out.println("Test");
     }
     
 }
